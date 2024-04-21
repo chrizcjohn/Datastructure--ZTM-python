fafb4eabc5658efbc3a544667b84a762f0837f30
Christin John <chrisjohn896@gmail.com>
2024-04-21 23:55:54 +0530

    ZTM-Hashtable

--- Data_Structures/Hashtables/RecurringCharacter.py
+++ Data_Structures/Hashtables/RecurringCharacter.py
@@ -1,16 +1,8 @@
 class RecurringCharacter:
 
-    def lookup_bruteforce(self, arrayList):
-        temp_list=[]
-        length = len(arrayList)
-        for i in range(length):
-            if arrayList[i] in temp_list:
-                return arrayList[i]
-            else:
-                temp_list.append(arrayList[i])
-        return None
 
-    def lookup_bruteforce2(self,arrayList):
+
+    def lookup_bruteforce(self, arrayList):
         length = len(arrayList)
         for i in range(length):
             for j in range(i+1,length):
@@ -30,7 +22,6 @@ class RecurringCharacter:
 if __name__ == '__main__':
 
     rec_obj = RecurringCharacter()
-    arrayList = [1, 2, 3, 10, 4, 7, 8, 9, 10]
+    arrayList = [1, 2, 2, 1, 4, 7, 8, 9, 10]
     print(rec_obj.lookup_bruteforce(arrayList))
-    print(rec_obj.lookup_bruteforce2(arrayList))
     print(rec_obj.lookup_non_bruteforce(arrayList))
