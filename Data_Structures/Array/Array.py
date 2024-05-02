8399d1c28a4b0a2e7c5f9f2a904da52a9a0df657
Christin John <chrisjohn896@gmail.com>
2024-05-02 23:31:43 +0530

    ZTM-Arrays

--- Data_Structures/Array/Array.py
+++ Data_Structures/Array/Array.py
@@ -19,6 +19,18 @@ class array:
         # print(str(self.__dict__))
         print(self.data)
 
+    def delete(self,index):
+        delete_item = self.get(index)
+        self.shiftitems(index)
+        self.length -= 1
+        return delete_item
+
+
+    def shiftitems(self,index):
+        for i in range(index,self.length-1):
+            self.data[i]=self.data[i+1]
+        del self.data[self.length-1]
+
 
 
 
@@ -26,8 +38,15 @@ if __name__ == "__main__":
     a = array()
     a.push('hi')
     a.push('there')
+    a.push('metal')
+    a.push('cat')
+    a.push('dog')
     print(a.get(0))
     print(a.get(1))
     print(a.pop())
+    a.delete(1)
+    a.push('monster')
+    a.delete(2)
     a.print_data()
+
 
