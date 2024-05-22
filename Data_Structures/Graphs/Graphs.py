600f9b04dd9a6c1a45929706dac7f8b23639b806
Christin John <chrisjohn896@gmail.com>
2024-05-23 00:21:49 +0530

    ZTM-Graph

--- Data_Structures/Graphs/Graphs.py
+++ Data_Structures/Graphs/Graphs.py
@@ -1,36 +1,31 @@
 class Graph:
     def __init__(self):
-        self.number_of_nodes=0;
-        self.adjacency_list={}
+        self.number_of_nodes = 0;
+        self.adjacency_list = {}
 
+    def add_vertex(self, node):
+        self.number_of_nodes += 1
+        self.adjacency_list[node] = []
 
-
-    def add_vertex(self,node):
-        self.number_of_nodes+=1
-        self.adjacency_list[node]=[]
-
-    def add_edge(self,node1,node2):
+    def add_edge(self, node1, node2):
         self.adjacency_list[node1].append(node2)
         self.adjacency_list[node2].append(node1)
 
     def show_connections(self):
-        for vertex,neighbours in self.adjacency_list.items():
+        for vertex, neighbours in self.adjacency_list.items():
             print(vertex, end='-->')
             print(' '.join(neighbours))
 
 
-
-if __name__=='__main__':
-    graph=Graph()
+if __name__ == '__main__':
+    graph = Graph()
     graph.add_vertex('1')
     graph.add_vertex('2')
     graph.add_vertex('3')
     graph.add_vertex('4')
-    graph.add_edge('1','2')
-    graph.add_edge('1','3')
-    graph.add_edge('1','4')
-    graph.add_edge('2','3')
-    graph.add_edge('2','4')
+    graph.add_edge('1', '2')
+    graph.add_edge('1', '3')
+    graph.add_edge('1', '4')
+    graph.add_edge('2', '3')
+    graph.add_edge('2', '4')
     graph.show_connections()
-
-
